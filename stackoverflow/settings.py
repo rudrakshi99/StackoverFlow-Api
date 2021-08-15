a5040fd1952af5947569f7d4658d876b62a20a70
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-08-15 21:46:30 +0530

    added postgres

--- stackoverflow/settings.py
+++ stackoverflow/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-64qyjls%)4$!dp+1nq-gp&w5_y&-1tt6^+s=io18(t3+9+dz#h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', '0.0.0.0', ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -89,10 +90,15 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stackdb',
+        'USER': 'postgres',
+        'PASSWORD':'password',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
@@ -136,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
