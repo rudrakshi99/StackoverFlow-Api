f2573e89a6bcfae16c7536ffb2e3bcf0d7c21053
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-08-16 13:09:39 +0530

    added env

--- stackoverflow/settings.py
+++ stackoverflow/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import environ
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-64qyjls%)4$!dp+1nq-gp&w5_y&-1tt6^+s=io18(t3+9+dz#h'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -94,9 +96,9 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'stackdb',
-        'USER': 'postgres',
-        'PASSWORD':'password',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
         'HOST':'localhost',
         'PORT':'5432'
     }
